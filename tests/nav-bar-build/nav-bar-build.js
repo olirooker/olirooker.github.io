dec3e684fdbec5cbf9d397ceb85acab456bf0f04
Oli Rooker <oliver.rooker@gmail.com>
2020-12-20 20:02:57 +0000

    add finished navbar desktop and mobile

--- tests/nav-bar-build/nav-bar-build.js
+++ tests/nav-bar-build/nav-bar-build.js
@@ -1,6 +1,17 @@
-navScroll = () => {
-  window.addEventListener("scroll", function () {
-    const navBar = document.querySelector("header");
-    navBar.classList.toggle("sticky", window.scrollY > 0);
-  });
+const menuNav = document.getElementById("mobile-nav");
+const desktopContactBtn = document.getElementById("contact-button");
+
+toggle = () => {
+  if (menuNav.classList.contains("mobile-nav-open")) {
+    menuNav.classList.remove("mobile-nav-open");
+  } else {
+    menuNav.classList.add("mobile-nav-open");
+  }
+};
+
+const navToggle = document.getElementById("toggle");
+
+toggleNav = () => {
+  const navToggle = document.getElementById("toggle");
+  navToggle.classList.toggle("active");
 };
